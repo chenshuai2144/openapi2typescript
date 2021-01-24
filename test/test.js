6338eb2fbd1ff26ecdb48a279e79e95807516e64
chenshuai2144 <qixian.cs@outlook.com>
2021-01-24 15:49:03 +0800

    support morse test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,19 @@
 const openAPI = require('../dist/index');
 
-openAPI.generateService({
-  schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-  serversPath: './servers',
-  mockFolder: './mocks',
-});
+// openAPI.generateService({
+//   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
+//   serversPath: './servers',
+//   mockFolder: './mocks',
+// });
+
+// openAPI.generateService({
+//   schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
+//   serversPath: './servers',
+//    mockFolder: './mocks',
+// });
 
 openAPI.generateService({
-  schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
+  schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/LyDMjDyIhK/1611471979478-opa.json',
   serversPath: './servers',
+  mockFolder: './mocks',
 });
