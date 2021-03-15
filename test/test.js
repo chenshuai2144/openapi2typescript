ce5d5224f50b7810a0a879cb02e4f45635a1c74c
chenshuai2144 <qixian.cs@outlook.com>
2021-03-15 14:39:25 +0800

    add more test

--- test/test.js
+++ test/test.js
@@ -1,37 +1,40 @@
 const openAPI = require('../dist/index');
 
-// openAPI.generateService({
-//   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-//   serversPath: './servers',
-//   mockFolder: './mocks',
-// });
+const gen = async () => {
+  // await openAPI.generateService({
+  //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
+  //   serversPath: './servers',
+  //   mockFolder: './mocks',
+  // });
 
-// openAPI.generateService({
-//   schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
-//   serversPath: './servers',
-//   mockFolder: './mocks',
-// });
+  // await openAPI.generateService({
+  //   schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
+  //   serversPath: './servers',
+  //   mockFolder: './mocks',
+  // });
 
-// openAPI.generateService({
-//   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/LyDMjDyIhK/1611471979478-opa.json',
-//   serversPath: './servers',
-//   mockFolder: './mocks',
-// });
+  // await openAPI.generateService({
+  //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/LyDMjDyIhK/1611471979478-opa.json',
+  //   serversPath: './servers',
+  //   mockFolder: './mocks',
+  // });
 
-// openAPI.generateService({
-//   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/Zd7dLTHUjE/ant-design-pro.json',
-//   serversPath: './servers',
-//   mockFolder: './mocks',
-// });
+  // await openAPI.generateService({
+  //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/Zd7dLTHUjE/ant-design-pro.json',
+  //   serversPath: './servers',
+  //   mockFolder: './mocks',
+  // });
 
-// openAPI.generateService({
-//   schemaPath: `${__dirname}/morse-api.json`,
-//   serversPath: './servers',
-//   mockFolder: './mocks',
-// });
+  // await openAPI.generateService({
+  //   schemaPath: `${__dirname}/morse-api.json`,
+  //   serversPath: './servers',
+  //   mockFolder: './mocks',
+  // });
 
-openAPI.generateService({
-  schemaPath: `${__dirname}/oc-swagger.json`,
-  serversPath: './servers',
-  mockFolder: './mocks',
-});
+  await openAPI.generateService({
+    schemaPath: `${__dirname}/oc-swagger.json`,
+    serversPath: './servers',
+    mockFolder: './mocks',
+  });
+};
+gen();
