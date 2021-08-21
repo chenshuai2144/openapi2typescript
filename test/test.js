6177d61347c297ee92bbb5d89e06495eb310c041
fifthai macbook pro <shadowcat.liuyang@gmail.com>
2021-08-21 21:03:21 +0800

    增加测试文件

--- test/test.js
+++ test/test.js
@@ -1,16 +1,20 @@
 const openAPI = require('../dist/index');
 
 const gen = async () => {
+  await openAPI.generateService({
+    schemaPath: `${__dirname}/swagger-allof.json`,
+    serversPath: './servers',
+  });
   // await openAPI.generateService({
   //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
   //   serversPath: './servers',
   //   mockFolder: './mocks',
   // });
-  await openAPI.generateService({
-    schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
-    serversPath: './servers',
-    mockFolder: './mocks',
-  });
+  // await openAPI.generateService({
+  //   schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
+  //   serversPath: './servers',
+  //   mockFolder: './mocks',
+  // });
   // await openAPI.generateService({
   //   schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/LyDMjDyIhK/1611471979478-opa.json',
   //   serversPath: './servers',
