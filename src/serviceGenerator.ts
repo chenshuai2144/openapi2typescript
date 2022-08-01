02b0f10d30b76c3f26712c6bfc68fc9b0796265d
程俊超 <chengjunchao@hotmail.com>
2022-08-01 18:43:33 +0800

    fix: 修复后台定义的名称为中文导致type name以number开头的报错的问题

--- src/serviceGenerator.ts
+++ src/serviceGenerator.ts
@@ -60,7 +60,12 @@ const resolveTypeName = (typeName: string) => {
     .replace(/[-_ ](\w)/g, (_all, letter) => letter.toUpperCase())
     .replace(/[^\w^\s^\u4e00-\u9fa5]/gi, '');
 
-  if (!/[\u3220-\uFA29]/.test(name)) {
+    // 当model名称是number开头的时候，ts会报错。这种场景一般发生在后端定义的名称是中文
+    if(name === '_' || /^\d$/.test(name)) {
+      Log('⚠️  models不能以number开头，原因可能是Model定义名称为中文, 建议联系后台修改');
+      return `Pinyin_${name}`
+    }
+  if (!/[\u3220-\uFA29]/.test(name) && !/^\d$/.test(name)) {
     return name;
   }
   const noBlankName = name.replace(/ +/g, '' )
