3c21a4cc47d7823d17db4f7c4afac1ee98bf5e34
huhm <huhm2008@foxmail.com>
2023-09-29 21:24:18 +0800

    feat: support customType and customFileNames hook (#120)
    * feat: support customType and customFileNames hook
    
    * feat: add afterOpenApiDataInited hook

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 import http from 'http';
 import https from 'https';
 import fetch from 'node-fetch';
-import type { OperationObject } from 'openapi3-ts';
+import type { OpenAPIObject, OperationObject, SchemaObject } from 'openapi3-ts';
 import converter from 'swagger2openapi';
 import Log from './log';
 import { mockGenerator } from './mockGenerator';
@@ -48,12 +48,63 @@ export type GenerateServiceProps = {
   projectName?: string;
 
   hook?: {
+    /** change open api data after constructor */
+    afterOpenApiDataInited?: (openAPIData: OpenAPIObject) => OpenAPIObject;
+
     /** 自定义函数名称 */
     customFunctionName?: (data: OperationObject) => string;
     /** 自定义类型名称 */
     customTypeName?: (data: OperationObject) => string;
     /** 自定义类名 */
     customClassName?: (tagName: string) => string;
+
+    /**
+     * 自定义获取type hook
+     * 返回非字符串将使用默认方法获取type
+     * @example set number to string
+     * function customType(schemaObject,namespace){
+     *  if(schemaObject.type==='number' && !schemaObject.format){
+     *    return 'BigDecimalString';
+     *  }
+     * }
+     */
+    customType?: (
+      schemaObject: SchemaObject | undefined,
+      namespace: string,
+      originGetType: (schemaObject: SchemaObject | undefined, namespace: string) => string,
+    ) => string;
+
+    /**
+     * 自定义生成文件名，可返回多个，表示生成多个文件
+     * 返回为空，则使用默认的获取方法获取
+     * @example  使用operationId生成文件名
+     * function customFileNames(operationObject,apiPath){
+     *   const operationId=operationObject.operationId;
+     *   if (!operationId) {
+     *      console.warn('[Warning] no operationId', apiPath);
+     *      return;
+     *    }
+     *    const res = operationId.split('_');
+     *    if (res.length > 1) {
+     *      res.shift();
+     *      if (res.length > 2) {
+     *        console.warn('[Warning]  operationId has more than 2 part', apiPath);
+     *      }
+     *      return [res.join('_')];
+     *    } else {
+     *      const controllerName = (res || [])[0];
+     *      if (controllerName) {
+     *        return [controllerName];
+     *      }
+     *      return;
+     *    }
+     * }
+     */
+    customFileNames?: (
+      operationObject: OperationObject,
+      apiPath: string,
+      _apiMethod: string,
+    ) => string[];
   };
   namespace?: string;
 
